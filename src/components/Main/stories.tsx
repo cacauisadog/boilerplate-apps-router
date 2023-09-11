ffa00d413ae7ece4564fd8fadd86c91f75b77601
cacauisadog <cbcaue@protonmail.com>
2023-09-11 19:39:56 -0300

    create first storybook story

---
+++ src/components/Main/stories.tsx
@@ -0,0 +1,12 @@
+import { Meta, StoryObj } from '@storybook/react'
+import Main from '.'
+
+export default {
+  title: 'Main',
+  parameters: {
+    layout: 'fullscreen'
+  },
+  component: Main
+} as Meta
+
+export const Default: StoryObj = {}
