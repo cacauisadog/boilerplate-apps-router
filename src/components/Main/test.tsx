a75ade8a5923d7518d1ee34b22b02a92ff64a963
cacauisadog <cbcaue@protonmail.com>
2023-09-11 19:13:07 -0300

    add and configure 'jest-styled-components' package

--- src/components/Main/test.tsx
+++ src/components/Main/test.tsx
@@ -13,4 +13,12 @@ describe('<Main />', () => {
     // gerar snapshot
     expect(container.firstChild).toMatchSnapshot()
   })
+
+  it('should render the colors correctly', () => {
+    // renderiza o componente
+    const { container } = render(<Main />)
+
+    // verificar se o background-color está correto
+    expect(container.firstChild).toHaveStyle({ 'background-color': '#06092b' })
+  })
 })
